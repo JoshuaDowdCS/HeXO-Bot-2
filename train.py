ccc913d1107fe2f463c162afb23d206bfea7c5b8
Joshua Dowd <160157080+AlexKhuel@users.noreply.github.com>
2026-03-27 20:52:40 -0700

    Fix NameError in train.py and update LESSONS.md

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import os
 import torch
-import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 from model import HeXONet
